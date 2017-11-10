9c5202e8fd432b2d2a3c063cba5e4f7d16528ead
thoughtbees <velusamyvnp@gmail.com>
2017-11-11 01:24:21 +0530

    login/register

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,8 +14,21 @@ class PagesController < ApplicationController
   def register
   end
 
+  def login_attempt
+    username = params[:username_or_email]
+    password = params[:password]
+    @user = Customer.find_by(email_id: username) if username.include? "@"
+    @user = Customer.find_by(first_name: username) unless username.include? "@"
+    if password == @user.password
+      session[:user_id]= @user.id
+      redirect_to pages_dashboard_path
+    else
+      redirect_to pages_login_path
+    end
+  end
   
-
   def logout
+    session[:user_id]=nil
+    redirect_to pages_dashboard_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+  get 'pages/dashboard'
+  
+    get 'pages/login'
+  
+    get 'pages/logout'
+  
+    get 'pages/contact'
+  
+    get 'pages/about'
+    
+    get 'pages/register'
+
+    post 'pages/login_attempt'
   resources :product_categories
   resources :orders
   resources :carts
@@ -9,17 +23,7 @@ Rails.application.routes.draw do
   resources :units
   resources :backend_users
   resources :roles
-  get 'pages/dashboard'
-
-  get 'pages/login'
-
-  get 'pages/logout'
-
-  get 'pages/contact'
-
-  get 'pages/about'
   
-  get 'pages/register'
   
 root "pages#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
